8b8aae9f97de4d8f596175171ff1ae2e59b63bb8
AnjanaB <meaagna@gmail.com>
2026-09-22 22:11:37 +0530

    Second commit

--- Sample.js
+++ Sample.js
@@ -1,1 +1,43 @@
-console.log("Hello World");
+let studentName="Anjana";
+console.log(studentName);
+//Store and print age
+let age=42;
+console.log(age);
+//store name and age 
+let name="Anju";
+let age1=34;
+console.log(name,age1)
+let country="India";
+console.log(country);
+let employeeName="Abc";
+console.log(employeeName);
+console.log(typeof(employeeName));
+let marks=25;
+console.log(typeof(marks))
+let age2=56
+age2=28
+console.log(age2)
+//store boolean value
+let isStudent=true
+console.log(typeof(isStudent))
+let collegeName="LMC"
+console.log(collegeName)
+let price=100
+let qty=3
+let total=price*qty
+console.log(total)
+let isActive=true
+console.log(isActive)
+let price1=99.50
+console.log(typeof(price1))
+//null
+let result=null
+console.log(result)
+console.log(typeof(result))
+let city=undefined
+console.log(typeof(city))
+let id=Symbol("id")
+console.log(typeof(id))
+let phoneNumber;
+console.log(phoneNumber)
+console.log(typeof(phoneNumber))
